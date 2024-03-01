f7e98f315a9d5d907063bae04a1565935afb9588
gichunga <gichungasteve6@gmail.com>
2024-03-02 02:07:40 +0300

    Implemented the RecipeService interface methods in the RecipeServiceImpl class

--- src/main/java/com/gichungasoftwares/superRecipes/service/RecipeService.java
+++ src/main/java/com/gichungasoftwares/superRecipes/service/RecipeService.java
@@ -9,7 +9,7 @@ public interface RecipeService {
     public Recipe createRecipe(Recipe recipe, User user);
     public Recipe findRecipeById(Long id) throws Exception;
     public void deleteRecipe(Long id) throws Exception;
-    public Recipe updateRecipe(Recipe recipe) throws Exception;
+    public Recipe updateRecipe(Recipe recipe, Long id) throws Exception;
     public List<Recipe> findAllRecipe();
-    public Recipe likeRecipe(Recipe recipe, User user) throws Exception;
+    public Recipe likeRecipe(Long recipeId, User user) throws Exception;
 }
---
+++ src/main/java/com/gichungasoftwares/superRecipes/service/RecipeServiceImpl.java
@@ -0,0 +1,76 @@
+package com.gichungasoftwares.superRecipes.service;
+
+import com.gichungasoftwares.superRecipes.model.Recipe;
+import com.gichungasoftwares.superRecipes.model.User;
+import com.gichungasoftwares.superRecipes.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    @Override
+    public Recipe createRecipe(Recipe recipe, User user) {
+        Recipe createdRecipe = new Recipe();
+        createdRecipe.setTitle(recipe.getTitle());
+        createdRecipe.setDescription(recipe.getDescription());
+        createdRecipe.setImage(recipe.getImage());
+        createdRecipe.setVegetarian(recipe.isVegetarian());
+        createdRecipe.setUser(user);
+        createdRecipe.setCreatedAt(LocalDateTime.now());
+
+        return recipeRepository.save(createdRecipe);
+    }
+
+    @Override
+    public Recipe findRecipeById(Long id) throws Exception {
+        Optional<Recipe> optionalRecipe = recipeRepository.findById(id);
+        if (optionalRecipe.isPresent()){
+            return optionalRecipe.get();
+        }
+        throw new Exception("Recipe with id " + id + " was not found");
+    }
+
+    @Override
+    public void deleteRecipe(Long id) throws Exception {
+        recipeRepository.deleteById(id);
+    }
+
+    @Override
+    public Recipe updateRecipe(Recipe recipe, Long id) throws Exception {
+        Recipe oldRecipe = findRecipeById(id);
+        if (recipe.getTitle() != null){
+            oldRecipe.setTitle(recipe.getTitle());
+        }
+        if (recipe.getDescription() != null){
+            oldRecipe.setDescription(recipe.getDescription());
+        }
+        if (recipe.getImage() != null){
+            oldRecipe.setImage(recipe.getImage());
+        }
+        return recipeRepository.save(oldRecipe);
+    }
+
+    @Override
+    public List<Recipe> findAllRecipe() {
+        return recipeRepository.findAll();
+    }
+
+    @Override
+    public Recipe likeRecipe(Long recipeId, User user) throws Exception {
+        Recipe recipe = findRecipeById(recipeId);
+        if (recipe.getLikes().contains(user.getId())){
+            recipe.getLikes().remove(user.getId());
+        }else {
+            recipe.getLikes().add(user.getId());
+        }
+        return null;
+    }
+}
