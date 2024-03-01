345989cea097c683343d7f56485d6ff234538138
gichunga <gichungasteve6@gmail.com>
2024-03-01 18:21:22 +0300

    initial project configuration

---
+++ src/main/java/com/gichungasoftwares/superRecipes/SuperRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.gichungasoftwares.superRecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gichungasoftwares/superRecipes/SuperRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gichungasoftwares.superRecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
